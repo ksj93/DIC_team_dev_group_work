e0b597ab120304617f433cb7e18a299a7902a6d2
yusei masuda <masuda.no@icloud.com>
2021-12-08 15:27:41 +0900

    ini

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_05_063935) do
+ActiveRecord::Schema.define(version: 2021_11_05_091617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "blogs", force: :cascade do |t|
-    t.string "title", null: false
-    t.text "content", null: false
-    t.bigint "user_id", null: false
+    t.string "title"
+    t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_blogs_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -31,5 +29,4 @@ ActiveRecord::Schema.define(version: 2021_11_05_063935) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "blogs", "users"
 end
