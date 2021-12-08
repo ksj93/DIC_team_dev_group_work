b9e67616809c7c960bd544ae295416978f97674f
ksj <ksjee17@gmail.com>
2021-12-08 15:35:26 +0900

    211208 modification conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: 'tops#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :sessions
+  resources :users
 end
